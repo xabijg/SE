d1625affdd404f5ebdf259541ddf144489b7806e
reibax <73133206+xabijg@users.noreply.github.com>
2023-03-27 15:46:46 +0200

    Main cambiado con m.de estados para interrupciones

--- main.c
+++ main.c
@@ -1,126 +1,134 @@
-
 #include "MKL46Z4.h"
 
-// Switches
-#define SW1_MASK (1 << 3)
-#define SW2_MASK (1 << 12)
+#define LED_GREEN_POS (5)
+#define LED_RED_POS (29)
+#define SW1_POS (3)
+#define SW2_POS (12)
 
-// // LED (RG)
-// // LED_GREEN = PTD5
-// // LED_RED = PTE29
+int SW1_ACUM = 0;
+int SW2_ACUM = 0;
+int CONTROL = -1;
 
-// // Sensor SW1 (puerto C bit 3)
-// // Sensor SW2 (puerto C bit 12)
-
-void delay(void)
-{
-  volatile int i;
-
-  for (i = 0; i < 1000000; i++);
-}
-
-
-void switch_init()
-{
-  SIM->COPC = 0;
-  SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;
-  PORTC->PCR[3] = PORT_PCR_MUX(1) | PORT_PCR_PE_MASK | PORT_PCR_PS_MASK;
-  PORTC->PCR[12] = PORT_PCR_MUX(1) | PORT_PCR_PE_MASK | PORT_PCR_PS_MASK;
-  GPIOC->PDDR &= ~(SW1_MASK | SW2_MASK);
-}
-
-// LED_GREEN = PTD5
 void led_green_init()
 {
-  SIM->COPC = 0;
-  SIM->SCGC5 |= SIM_SCGC5_PORTD_MASK;
-  PORTD->PCR[5] = PORT_PCR_MUX(1);
-  GPIOD->PDDR |= (1 << 5);
-  GPIOD->PSOR = (1 << 5);
+  SIM->COPC = 0;                  
+  SIM->SCGC5 |= SIM_SCGC5_PORTD_MASK;       
+  PORTD->PCR[LED_GREEN_POS] = PORT_PCR_MUX(1);  
+  GPIOD->PDDR |= (1 << LED_GREEN_POS);      
+  GPIOD->PSOR |= (1 << LED_GREEN_POS);    
 }
 
-void led_green_on()
+void led_green_on(void)
 {
-  GPIOD->PCOR = (1 << 5);
+  GPIOD->PCOR |= (1 << LED_GREEN_POS);
 }
 
-void led_green_off()
-{
-  GPIOD->PSOR = (1 << 5);
+void led_green_off(void) {
+  GPIOD->PSOR |= (1 << LED_GREEN_POS);
 }
 
-void led_green_toggle()
-{
-  GPIOD->PTOR = (1 << 5);
-}
-
-// LED_RED = PTE29
-void led_red_init()
-{
+void led_red_init() {
   SIM->COPC = 0;
   SIM->SCGC5 |= SIM_SCGC5_PORTE_MASK;
-  PORTE->PCR[29] = PORT_PCR_MUX(1);
-  GPIOE->PDDR |= (1 << 29);
-  GPIOE->PSOR = (1 << 29);
+  PORTE->PCR[LED_RED_POS] = PORT_PCR_MUX(1);
+  GPIOE->PDDR |= (1 << LED_RED_POS);
+  GPIOE->PSOR |= (1 << LED_RED_POS);
 }
 
-void led_red_on(void)
-{
-  GPIOE->PCOR = (1 << 29);
+void led_red_on(void) {
+  GPIOE->PCOR |= (1 << LED_RED_POS);
 }
 
-void led_red_off(void)
-{
-  GPIOE->PSOR = (1 << 29);
+void led_red_off(void) {
+  GPIOE->PSOR |= (1 << LED_RED_POS);
 }
 
-void led_red_toggle(void)
-{
-  GPIOE->PTOR = (1 << 29);
+
+void sw1_init() {
+  
+  SIM->COPC = 0;
+  SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;
+  PORTC->PCR[SW1_POS] |= PORT_PCR_MUX(1);
+  PORTC->PCR[SW1_POS] |= PORT_PCR_PE_MASK;// Pull enable
+  PORTC->PCR[SW1_POS] |= PORT_PCR_PS_MASK;// Pull select
+
+  PORTC->PCR[SW1_POS] |= PORT_PCR_IRQC(0xA); // IRQ en el flanco de bajada
+  NVIC_SetPriority(31, 0);  // Interrupcion 31
+  NVIC_EnableIRQ(31);     // La activa
 }
+
+void sw2_init() {
+
+  SIM->COPC = 0;
+  SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;
+  PORTC->PCR[SW2_POS] |= PORT_PCR_MUX(1);
+  PORTC->PCR[SW2_POS] |= PORT_PCR_PE_MASK;
+  PORTC->PCR[SW2_POS] |= PORT_PCR_PS_MASK;
+
+  // IRQ
+  PORTC->PCR[SW2_POS] |= PORT_PCR_IRQC(0xA);
+  NVIC_SetPriority(31, 0);
+  NVIC_EnableIRQ(31);
+}
+
+void PORTDIntHandler(void) {
+  int pressed = PORTC->ISFR;
+  PORTC->ISFR = 0xFFFFFFFF; // Limpiamos
+
+  if(pressed == (0x8)) 
+  {
+    SW1_ACUM += 1;
+    if(SW1_ACUM > 1) 
+    {
+      SW1_ACUM = 0;
+    }
+  }
+
+  if(pressed == (0x1000)) 
+  {
+    SW2_ACUM += 1;
+    if(SW2_ACUM > 1) 
+    {
+      SW2_ACUM = 0;
+    }
+  }
+
+  CONTROL = SW1_ACUM + (SW2_ACUM * 2);
+}
+
 
 int main(void)
 {
   led_green_init();
   led_red_init();
-  switch_init();
+  sw1_init();
+  sw2_init();
 
-  int sw1_count = 0;
-  int sw2_count = 0;
-
-  while (1) {
-    if (!(GPIOC->PDIR & SW1_MASK)) {
-      delay(); 
-      if (!(GPIOC->PDIR & SW1_MASK)) {
-        sw1_count++;
-        if (sw1_count >= 2) {
-          sw1_count = 0;
-          led_red_on();
-        }
-      }
-    }
-
-  
-    if (!(GPIOC->PDIR & SW2_MASK)) {
-      delay(); 
-      if (!(GPIOC->PDIR & SW2_MASK)) { 
-        sw2_count++;
-        if (sw2_count >= 2) {
-          sw2_count = 0;
-          led_red_on();
-        }
-      }
-    }
-
-    
-    if (sw1_count == 1 || sw2_count == 1) {
-      led_green_off();
-      led_red_on();
-    } else {
-      led_green_on();
-      led_red_off();
+  while(1) {
+     
+    // 00 = 0 Ambas cerradas
+    // 01 = 1 Puerta 2 abierta
+    // 10 = 2 Puerta 1 abierta
+    // 11 = 3 Ambas abiertas
+     
+    switch(CONTROL) {
+      case 0:
+        led_green_on();
+        led_red_off();
+        break;
+      case 1:
+        led_green_off();
+        led_red_on();
+        break;
+      case 2:
+        led_green_off();
+        led_red_on();
+        break;
+      case 3:
+        led_green_off();
+        led_red_on();
+        break;
     }
   }
-
   return 0;
 }
